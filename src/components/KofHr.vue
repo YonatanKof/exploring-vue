33af11434a473cf6690dda1f153b07a8187695f7
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-09-19 17:28:01 +0300

    Add Hr componenet

---
+++ src/components/KofHr.vue
@@ -0,0 +1,36 @@
+<template>
+  <hr :style="cssVars"/>
+</template>
+
+<script>
+export default {
+  name: "KofHr",
+  props: {
+    hrHeight: {
+      default: "1px",
+      type: String,
+    },
+    hrMargin: {
+      default: "1rem",
+      type: String,
+    },
+  },
+  computed: {
+    cssVars() {
+      return {
+        "--hr-height": this.hrHeight,
+        "--hr-margin": this.hrMargin,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+hr {
+  border: var(--hr-height) solid var(--color-system);
+  border-radius: var(--hr-height);
+  margin-top: var(--hr-margin);
+  margin-bottom: var(--hr-margin);
+}
+</style>
