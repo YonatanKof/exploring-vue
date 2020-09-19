8d46ac5446b74d23df5682584969700a8d34d57b
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-09-19 14:29:18 +0300

    Add firts experiment

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChangeCss msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChangeCss from './components/ChangeCss.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChangeCss
   }
 }
 </script>
---
+++ src/components/ChangeCss.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div
+      id="change-color"
+      v-on:click="dange = !dange"
+      v-bind:class="{kofClass: kofClass, dange: dange}"
+    >
+      <h3>Hello my love</h3>
+    </div>
+    <div :class="compClasses">
+      <button v-on:click="titles = !titles">Toggle Color</button>
+      <button v-on:click="extraTitles = !extraTitles">Toggle Width</button>
+      <h3>Hell yes</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      kofClass: true,
+      dange: false,
+      titles: false,
+      extraTitles: false,
+    };
+  },
+  computed: {
+    compClasses: function () {
+      return {
+        titles: this.titles,
+        extraTitles: this.extraTitles,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  font-size: 16px;
+  --title-3: 4rem;
+  --color-caution: red;
+  --color-success: blue;
+}
+.kofClass {
+  cursor: pointer;
+  color: var(--color-success);
+}
+.dange {
+  color: var(--color-caution);
+}
+h3 {
+  font-weight: 100;
+  font-size: var(--title-3);
+  margin: 0.25em;
+  transition: font-weight 0.5s, color 1s;
+}
+.titles {
+  h3 {
+    color: var(--color-caution);
+  }
+}
+.extraTitles {
+  h3 {
+    font-weight: 900;
+  }
+}
+</style>
