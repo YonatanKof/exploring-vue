c285fe72e69bf96d48e11fcd7cf100386243f79e
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-09-19 17:31:10 +0300

    Add ExperimentTwo

---
+++ src/components/ExperimentTwo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>{{ name }}</h3>
+    <h3>{{ job }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExperimentTwo",
+  props: {
+    name: {
+      default: "Yonatan Kof",
+      type: String,
+    },
+    job: {
+      default: "Designer",
+      type: String,
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
